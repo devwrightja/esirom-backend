141df364208f4c0898680ac3627e90b0b2510650
Leon Wright <lwright@devwright.com>
2016-10-27 11:12:48 -0500

    change database ip

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ var insertSubscriber = function(db, callback, req, res) {
 
 /* GET users listing. */
 router.post('/subscribe', function(req, res) {
-  var url = process.env.MONGODB_IP || 'mongodb://localhost:27017/esirom';
+  var url = process.env.MONGODB_IP;
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server.");
